30c0838528cc09eb4419d796f8e04f798f47a643
Jeton Thaçi <jetonthaci@outlook.com>
2019-07-12 15:01:35 +0200

    place a ship in board

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,8 +1,18 @@
 const gameBoard = () => {
-  const board = [];
-  const placeShip = (shipLength) => {
-   board.push(shipLength)
+  const board = new Array(100).fill(0);
+
+  const placeShip = (ship) => {
+    let start = Math.floor((Math.random() * 99 - ship.size) + 0);
+    while (board[start] === 1) {
+      start = Math.floor((Math.random() * 99 - ship.size) + 0);
+    }
+
+    for (let i = start; i < ship.size; i += 1) {
+      console.log(i);
+      board[i] = 1;
+    }
   };
+
   return {
     board,
     placeShip,
--- tests/gameBoard.test.js
+++ tests/gameBoard.test.js
@@ -1,16 +1,13 @@
 const gameBoard = require('../src/gameBoard');
 const ship = require('../src/ship');
 
+const myBoard = gameBoard();
 const patrol = ship(2);
+myBoard.placeShip(patrol);
 
-const newBoard = gameBoard();
 
-test('Game board returning an array', () => {
-  expect(newBoard.board).toEqual([]);
+test('Game board has correctly placed ship', () => {
+  const count = myBoard.board.filter(a => a === 1);
+  console.log(myBoard.board);
+  expect(count).toBe(2);
 });
-
-test('Should place patrol boat with 2 size in the board', () => {
-    newBoard.placeShip(patrol.size);
-    expect(newBoard.board).toContain(2);
-  });
-  
