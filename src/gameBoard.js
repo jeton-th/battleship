86b60964ca111c6365c67bcac501b5057fdb9511
Jeton Thaçi <jetonthaci@outlook.com>
2019-07-12 15:39:38 +0200

    add test for placeShip

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,15 +1,15 @@
 const gameBoard = () => {
-  const board = new Array(100).fill(0);
+  const board = new Array(10).fill(0);
 
   const placeShip = (ship) => {
-    let start = Math.floor((Math.random() * 99 - ship.size) + 0);
+    let start = Math.floor((Math.random() * (99 - ship.size)) + 0);
     while (board[start] === 1) {
-      start = Math.floor((Math.random() * 99 - ship.size) + 0);
+      start = Math.floor((Math.random() * (99 - ship.size)) + 0);
     }
 
-    for (let i = start; i < ship.size; i += 1) {
-      console.log(i);
-      board[i] = 1;
+    const end = start + ship.size;
+    for (start; start < end; start += 1) {
+      board[start] = 1;
     }
   };
 
--- tests/gameBoard.test.js
+++ tests/gameBoard.test.js
@@ -3,11 +3,16 @@ const ship = require('../src/ship');
 
 const myBoard = gameBoard();
 const patrol = ship(2);
+const cruiser = ship(5);
 myBoard.placeShip(patrol);
+myBoard.placeShip(cruiser);
 
 
 test('Game board has correctly placed ship', () => {
-  const count = myBoard.board.filter(a => a === 1);
-  console.log(myBoard.board);
-  expect(count).toBe(2);
+  const count = myBoard.board.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0,
+  );
+  console.log(myBoard.board)
+  expect(count).toBe(7);
 });
