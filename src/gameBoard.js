609df357fc2b3079b6df54b82327acf021cedef6
Jeton Thaçi <jetonthaci@outlook.com>
2019-07-15 12:39:50 +0200

    testing placeShip function

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,16 +1,46 @@
 const gameBoard = () => {
-  const board = new Array(10).fill(0);
-
+  const board = Array.from(Array(10), () => Array(10).fill(0));
   const placeShip = (ship) => {
-    let start = Math.floor((Math.random() * (99 - ship.size)) + 0);
-    while (board[start] === 1) {
-      start = Math.floor((Math.random() * (99 - ship.size)) + 0);
+    const randomNumber = (min, max) => Math.floor((Math.random() * max) + min);
+
+    let direction = (randomNumber(1, 2) === 1) ? 'h' : 'v';
+    let x = randomNumber(0, 9 - ship.size);
+    let y = randomNumber(0, 9 - ship.size);
+
+    function place() {
+      let i = (direction === 'h') ? x : y;
+      let j = (direction === 'h') ? y : x;
+      const end = i + ship.size;
+
+      let occupied = false;
+      for (let k = i; k < end; k += 1) {
+        if (direction === 'h') {
+          occupied = (board[k][j] === 1);
+        } else {
+          occupied = (board[j][k] === 1);
+        }
+      }
+
+      if (occupied) {
+        direction = (randomNumber(1, 2) === 1) ? 'h' : 'v';
+        x = randomNumber(0, 9 - ship.size);
+        y = randomNumber(0, 9 - ship.size);
+        place();
+      } else {
+        for (i; i < end; i += 1) {
+          console.log(`i: ${i} - j: ${j} - direction: ${direction} - shi size: ${ship.size}`)
+          if (direction === 'h') {
+            board[j][i] = 1;
+          } else {
+            board[i][j] = 1;
+          }
+        }
+      }
     }
 
-    const end = start + ship.size;
-    for (start; start < end; start += 1) {
-      board[start] = 1;
-    }
+    place();
+
+    console.log(board)
   };
 
   return {
--- tests/gameBoard.test.js
+++ tests/gameBoard.test.js
@@ -2,17 +2,18 @@ const gameBoard = require('../src/gameBoard');
 const ship = require('../src/ship');
 
 const myBoard = gameBoard();
-const patrol = ship(2);
-const cruiser = ship(5);
-myBoard.placeShip(patrol);
-myBoard.placeShip(cruiser);
 
+const a = ship(5);
+const b = ship(5);
+const c = ship(5);
+const d = ship(5);
+
+myBoard.placeShip(a);
+myBoard.placeShip(b);
+myBoard.placeShip(c);
+myBoard.placeShip(d);
 
 test('Game board has correctly placed ship', () => {
-  const count = myBoard.board.reduce(
-    (accumulator, currentValue) => accumulator + currentValue,
-    0,
-  );
-  console.log(myBoard.board)
-  expect(count).toBe(7);
+  const count = (myBoard.board.filter(a => a === 1)).count;
+  expect(count).toBe(5);
 });
