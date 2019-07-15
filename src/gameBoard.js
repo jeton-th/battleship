190fe16a203b03ba11cfb185879d3c490cf0d358
Zeha Irawan <jangkaringbumi@gmail.com>
2019-07-15 19:27:27 +0700

    optimize placeShips

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,31 +1,36 @@
 const gameBoard = () => {
   const board = Array.from(Array(10), () => Array(10).fill(0));
-  const placeShip = (ship) => {
-    const randomNumber = (min, max) => Math.floor((Math.random() * max) + min);
+  const placeShip = ship => {
+    const randomNumber = (min, max) => Math.floor(Math.random() * max + min);
 
-    let direction = (randomNumber(1, 2) === 1) ? 'h' : 'v';
-    let x = randomNumber(0, 9 - ship.size);
-    let y = randomNumber(0, 9 - ship.size);
+    let direction = randomNumber(1, 2) === 1 ? 'h' : 'v';
+    let x = randomNumber(0, 9);
+    let y = randomNumber(0, 9);
 
     function place() {
-      let i = (direction === 'h') ? x : y;
-      let j = (direction === 'h') ? y : x;
+      let i, j;
+      if (direction === 'h') {
+        i = x;
+        j = y;
+      } else {
+        i = y;
+        j = x;
+      }
+
+      i = i + ship.size > 9 ? 9 - ship.size : i;
       const end = i + ship.size;
 
       let occupied = false;
       for (let k = i; k < end; k += 1) {
         if (direction === 'h') {
-          occupied = (board[j][k] === 1) ? true : occupied;
+          occupied = board[j][k] === 1 ? true : occupied;
         } else {
-          occupied = (board[k][j] === 1) ? true : occupied;
+          occupied = board[k][j] === 1 ? true : occupied;
         }
-        console.log(`i: ${i} - j: ${j} - direction: ${direction} - occup: ${occupied}`)
-        console.log(`k: ${k} - j: ${j} - direction: ${direction} - occup: ${occupied}`)
       }
 
-
       if (occupied) {
-        direction = (randomNumber(1, 2) === 1) ? 'h' : 'v';
+        direction = randomNumber(1, 2) === 1 ? 'h' : 'v';
         x = randomNumber(0, 9 - ship.size);
         y = randomNumber(0, 9 - ship.size);
         place();
@@ -41,13 +46,11 @@ const gameBoard = () => {
     }
 
     place();
-
-    console.log(board)
   };
 
   return {
     board,
-    placeShip,
+    placeShip
   };
 };
 
