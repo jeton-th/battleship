3330f6266b2c303035d7a5018d50d606b918910a
Jeton Thaçi <jetonthaci@outlook.com>
2019-07-17 13:00:52 +0200

    try to add numbers and letters to boards

--- src/dom.js
+++ src/dom.js
@@ -2,9 +2,27 @@
 
 function createBoard(board, parentDiv) {
   const domBoard = document.querySelector(`.${parentDiv}`);
+  const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
   board.forEach((row, i) => {
+    if (i === 0) {
+      for (let j = 0; j < 10; j += 1) {
+        const letter = document.createElement('span');
+        letter.classList.add('box');
+        letter.innerHTML = letters[j];
+        domBoard.appendChild(letter);
+      }
+    }
     row.forEach((box, j) => {
+      if (j === 0) {
+        // for (let j = 0; j < 10; j += 1) {
+        const letter = document.createElement('span');
+        letter.classList.add('box');
+        letter.innerHTML = i;
+        domBoard.appendChild(letter);
+        // }
+      }
       const button = document.createElement('button');
+      button.classList.add('box');
       button.id = `${parentDiv}-${i}-${j}`;
       domBoard.appendChild(button);
     });
@@ -16,9 +34,9 @@ function createBoard(board, parentDiv) {
 function changeButton(button, res) {
   button.disabled = true;
   if (res === 'hit') {
-    button.style.background = 'red';
+    button.style.background = 'crimson';
   } else {
-    button.style.background = 'blue';
+    button.style.background = 'lightblue';
   }
 }
 
